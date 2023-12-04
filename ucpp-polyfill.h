2e141580b0ae935f84a9cabd3b596dc0d83afc52
Jasmine Tai <jtai@jtai.ca>
2023-12-04 17:09:18 -0500

    Add polyfill for delivery()

--- ucpp-polyfill.h
+++ ucpp-polyfill.h
@@ -2,6 +2,15 @@
 
 #include <cstdint>
 
+// Include these first, so that we don't mess up the delivery() functions inside
+#include "uCPP/source/src/library/uActor.h"
+#include "uCPP/source/src/library/uFuture.h"
+
+/// Utilities
+namespace {
+template <typename T> T __convert(...) { throw 0; }
+} // namespace
+
 /// @brief Yields to the scheduler.
 /// @note This is a static member function of `uBaseTask`.
 void yield();
@@ -10,3 +19,5 @@ void yield();
 /// @param times number of times to yield
 /// @note This is a static member function of `uBaseTask`.
 void yield(size_t times);
+
+#define delivery(...) delivery(__convert<uBaseEvent*>(__VA_ARGS__))
